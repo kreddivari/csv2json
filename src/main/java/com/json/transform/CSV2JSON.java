ab32f0af713a7c1ae7e3120806cd40a6475d4ac8
kreddiva <kalyanreddy.reddivari1986@gmail.com>
2018-09-04 22:37:13 -0400

    initial
    initial

---
+++ src/main/java/com/json/transform/CSV2JSON.java
@@ -0,0 +1,31 @@
+package com.json.transform;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Map; 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+public class CSV2JSON {
+	
+
+	 
+    public static String convertCSVToJSON (InputStream csv) throws Exception {
+       // File input = new File("src/main/resources/sample.csv");
+        File output = new File("output.json");
+        String result_json=null;
+        CsvSchema csvSchema = CsvSchema.builder().setUseHeader(true).build();
+        CsvMapper csvMapper = new CsvMapper();
+        List<Object> readAll = csvMapper.readerFor(Map.class).with(csvSchema).readValues(new InputStreamReader(csv, "ISO-8859-1")).readAll();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writerWithDefaultPrettyPrinter().writeValue(output, readAll);
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(readAll));
+        result_json=mapper.writerWithDefaultPrettyPrinter().writeValueAsString(readAll);
+        return result_json;
+}
+
+}
